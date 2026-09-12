c25b32c1f804e02e714787a80ac7d9d778a9bf34
MahmoudMostafa01 <mahmoudmostafaworks@gmail.com>
2026-09-12 19:41:20 +0300

    add uv to  ingestion_layer

--- ingestion_layer/buffer_test.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-
-print("Starting the test...")
-for i in range(1, 6):
-    print(f"Log message {i} - waiting...")
-    # We sleep to show that the text DOES NOT appear immediately
-    time.sleep(1) 
-print("Finished!")
